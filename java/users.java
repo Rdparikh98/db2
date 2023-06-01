2c767e00c27beecc19b23b503aea000ca68155af
Jinal2626 <133625084+Jinal2626@users.noreply.github.com>
2023-06-01 17:00:00 +1000

    Update users.java

--- java/users.java
+++ java/users.java
@@ -1,1 +1,86 @@
+package scias;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+
+public class Users extends JFrame {
+
+	private JPanel contentPane;
+	private JTable userstbl;
+	private JButton btnNewButton_2;
+	private JScrollPane scrollPane;
+
+
+	public Users() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 652, 438);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(231, 11, 397, 379);
+		contentPane.add(scrollPane);
+		
+		userstbl = new JTable();
+		scrollPane.setViewportView(userstbl);
+		
+		JButton btnNewButton = new JButton("Add New User");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new User();
+			}
+		});
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnNewButton.setBounds(30, 158, 141, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Back");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new admindashboard();
+			}
+		});
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnNewButton_1.setBounds(30, 241, 141, 23);
+		contentPane.add(btnNewButton_1);
+		
+		btnNewButton_2 = new JButton("View Users");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Connection con;
+				try {
+					//
+					while(rs.next()) {
+						//
+					}
+					st.close();
+					con.close();
+				}catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnNewButton_2.setBounds(30, 66, 141, 23);
+		contentPane.add(btnNewButton_2);
+	}
+
+}
 
